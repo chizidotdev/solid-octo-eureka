b17703728a518e145aaeddbcd31f9ce25f283958
chiziivictor <chizivwokoma@gmail.com>
2022-11-30 23:23:39 +0100

    feat: enable nested layout feature

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,32 @@
 import React from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+
 import { ServiceProvider } from "@store/service";
 import { UserProvider } from "@store/user";
 import { ThemeProvider } from "src/store/theme";
+
 import "../styles/color.css";
 import "../styles/font.css";
 import "../styles/index.css";
 import "../styles/tailwind.css";
 
-export default function MyApp({ Component, pageProps }) {
-  return (
+export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<
+  P,
+  IP
+> & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
     <ThemeProvider>
       <UserProvider>
         <ServiceProvider>
