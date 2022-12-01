aeb521b36a1123992c3e1fa9ca5ee9a06157adbe
chiziivictor <chizivwokoma@gmail.com>
2022-12-01 12:59:30 +0100

    style: add active styles on main navigation

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <>
       <nav className="fixed bottom-0 z-10 flex h-20 w-full items-center justify-center gap-8 bg-white_day dark:bg-white_night md:sticky md:h-auto md:w-32 md:flex-col md:justify-start md:pt-[20vh] xl:w-[10vw]">
         {icons.map((icon) => (
-          <Link href={icon.href} key={icon.src}>
+          <Link href={`/${icon.href}`} key={icon.src}>
             <Icon alt={icon.alt} src={icon.src} href={icon.href} />
           </Link>
         ))}
@@ -19,7 +19,8 @@ export default function Navbar() {
 
 const Icon = ({ src, alt, href }: typeof icons[0]) => {
   const { pathname } = useRouter();
-  const active = href === pathname;
+  // const active = href === pathname;
+  const active = href !== "" && pathname.includes(href);
 
   return (
     <div className={`rounded-full p-4 ${active ? "bg-gray_day" : ""}`}>
@@ -35,8 +36,8 @@ const Icon = ({ src, alt, href }: typeof icons[0]) => {
 };
 
 const icons = [
-  { src: "img_home.svg", alt: "home", href: "/" },
-  { src: "img_user.svg", alt: "users", href: "/users" },
-  { src: "img_grid.svg", alt: "services", href: "/services" },
-  { src: "img_settings.svg", alt: "settings", href: "/settings" },
+  { src: "img_home.svg", alt: "home", href: "" },
+  { src: "img_user.svg", alt: "users", href: "users" },
+  { src: "img_grid.svg", alt: "services", href: "services" },
+  { src: "img_settings.svg", alt: "settings", href: "settings" },
 ];
