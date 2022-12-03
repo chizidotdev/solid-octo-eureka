eba6c253330385471a5c6a674f9a92a16c9d8a86
chiziivictor <chizivwokoma@gmail.com>
2022-12-03 18:18:28 +0100

    fix: modify getLayout export on settings to show settings layout

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -2,9 +2,9 @@ import type { NextPageWithLayout } from "../_app";
 import Layout from "@components/layout";
 
 const Settings: NextPageWithLayout = () => {
-  return <></>;
+  return null;
 };
 
-Settings.getLayout = (page) => <Layout settings>{page}</Layout>;
+Settings.getLayout = () => <Layout settings>{undefined}</Layout>;
 
 export default Settings;
