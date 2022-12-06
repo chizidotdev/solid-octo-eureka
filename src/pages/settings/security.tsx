c8edcd78a769d1455732801cca2f7afc82b115d2
chiziivictor <chizivwokoma@gmail.com>
2022-12-06 12:52:26 +0100

    feat: create change password form on settings security page

--- src/pages/settings/security.tsx
+++ src/pages/settings/security.tsx
@@ -1,11 +1,55 @@
 import Layout from "@components/layout";
-import Text from "@ui/text";
+import { Button, Input, Text } from "@components/ui";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { FiCheck } from "react-icons/fi";
 import type { NextPageWithLayout } from "../_app";
 
 const Security: NextPageWithLayout = () => {
+  const [password, setPassword] = useState("");
+  const { push } = useRouter();
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleDone = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    push("/settings");
+  };
+
   return (
     <>
-      <Text intent="h2">Security</Text>
+      <Text intent="h2">Reset Password</Text>
+
+      <form className="flex max-w-lg flex-col gap-5">
+        <label className="grid gap-2 text-sm">
+          Password
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <label className="grid gap-2 text-sm">
+          Confirm Password
+          <Input
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+
+        <div className="mt-10 self-end">
+          <Button size="small" variant="rounded" onClick={handleDone}>
+            <span className="grid grid-cols-[1fr_6fr] place-items-center gap-5 pr-5">
+              <FiCheck />
+              Done
+            </span>
+          </Button>
+        </div>
+      </form>
     </>
   );
 };
