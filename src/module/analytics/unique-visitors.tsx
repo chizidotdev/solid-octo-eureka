128d484a25e7318f1d89cce1a0adffe0bd6b022b
chizidotdev <chizivwokoma@gmail.com>
2022-12-13 15:01:21 +0100

    feat: initialize unique visitors line chart

---
+++ src/module/analytics/unique-visitors.tsx
@@ -0,0 +1,40 @@
+import { randNumber } from "@ngneat/falso";
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => randNumber({ min: 0, max: 300 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => randNumber({ min: 0, max: 300 })),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export function UniqueVisitors() {
+  return <Line options={options} data={data} />;
+}
