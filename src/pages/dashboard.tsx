95453c4f311640a05ddd10de8c50743f17714ee3
chizidotdev <chizivwokoma@gmail.com>
2022-12-13 16:03:26 +0100

    feat: implement new analytics charts to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,20 +1,61 @@
 import React from "react";
 import type { NextPageWithLayout } from "./_app";
 import Layout from "@components/layout";
-import { AnalyticsCard, UserRegistration } from "@module/analytics";
-import type { AnalyticsCardProps } from "@module/analytics";
 import { FiUsers, AiOutlineShopping, FaUsers } from "@icons";
+import {
+  AnalyticsCard,
+  Navigation,
+  SessionsPerDay,
+  UniqueVisitors,
+  UserByDevice,
+  UserRegistration,
+  UsersByGender,
+} from "@module/analytics";
+import type { AnalyticsCardProps } from "@module/analytics";
+import { Text } from "@components/ui";
 
 const Dashboard: NextPageWithLayout = () => {
   return (
-    <div className="grid w-3/4 gap-10">
-      <div className="flex gap-6">
-        {summaryData.map((summary) => (
-          <AnalyticsCard {...summary} key={summary.title} />
-        ))}
-      </div>
+    <div className="grid gap-32">
+      <section className="grid grid-cols-[3fr_1fr] gap-20">
+        <div>
+          <div className="flex gap-6">
+            {summaryData.map((summary) => (
+              <AnalyticsCard {...summary} key={summary.title} />
+            ))}
+          </div>
 
-      <UserRegistration />
+          <div className="mt-14 flex w-2/3 flex-col gap-3">
+            <Text intent="h3">User Registration</Text>
+
+            <UserRegistration />
+          </div>
+        </div>
+      </section>
+
+      <section className="grid grid-cols-2 gap-20">
+        <div>
+          <UniqueVisitors />
+        </div>
+
+        <div>
+          <SessionsPerDay />
+        </div>
+      </section>
+
+      <section className="grid grid-cols-3 gap-20">
+        <div>
+          <UsersByGender />
+        </div>
+
+        <div>
+          <Navigation />
+        </div>
+
+        <div>
+          <UserByDevice />
+        </div>
+      </section>
     </div>
   );
 };
@@ -37,9 +78,9 @@ const summaryData: AnalyticsCardProps[] = [
     icon: <AiOutlineShopping />,
   },
   {
-    title: "Rhendors",
-    percentage: -1.43,
-    value: 1986,
+    title: "Admin",
+    percentage: -3.56,
+    value: 5,
     icon: <FaUsers />,
   },
 ];
