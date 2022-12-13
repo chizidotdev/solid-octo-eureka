a97fc234bb0222a54b2b64f34193dbbff17571d2
chizidotdev <chizivwokoma@gmail.com>
2022-12-13 15:02:32 +0100

    feat: initialize sessions per day area chart

---
+++ src/module/analytics/sessions-per-day.tsx
@@ -0,0 +1,34 @@
+import { randNumber } from "@ngneat/falso";
+import { Line } from "react-chartjs-2";
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: "Dataset 2",
+      data: labels.map(() => randNumber({ min: 0, max: 1000 })),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export function SessionsPerDay() {
+  return <Line options={options} data={data} />;
+}
